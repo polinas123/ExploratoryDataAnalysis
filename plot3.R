3ed7f5a099c2c8175b6cc37246fb84987afd031c
polinas123 <polinas86@gmail.com>
2017-06-12 12:47:44 +0300

    updated some plots to ggplot2

--- plot3.R
+++ plot3.R
@@ -1,16 +1,30 @@
-source("read_data.R")
+if (!require(data.table)){
+        install.packages("data.table")
+        require(data.table)
+}
 
-# read data into data tables
-data <- readData()
-emissions <- data[[1]]
-css <- data[[2]]
+if (!require(ggplot2)){
+        install.packages("ggplot2")
+        require(ggplot2)
+}
+
+# downliad data
+URL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(url = URL, destfile = "NEI.zip")
+# read files
+data <- unzip(zipfile = "NEI.zip")
+emissions <- readRDS(data[grep(x = data, "summarySCC_PM25.rds")])
+scc <- readRDS(data[grep(x = data, "Source_Classification_Code.rds")])
+
+emissions <- as.data.table(emissions)
+scc <- as.data.table(scc)
 
 # # calculate sum of emission for each year for Baltimore city - by type
 ts <- emissions[fips=="24510", lapply(.SD,sum), by = list(year, type), .SDcols = "Emissions"]
 
-setkey(ts, "type")
+d <- as.data.frame(ts)
 
 # plot with ggplot:
 png(filename = "plot3.png")
-
+qplot(year, Emissions, data = d, color = type) + geom_line()
 dev.off()
--- plot6.R
+++ plot6.R
@@ -1,9 +1,18 @@
-source("read_data.R")
+if (!require(data.table)){
+        install.packages("data.table")
+        require(data.table)
+}
 
-# read data into data tables
-data <- readData()
-emissions <- data[[1]]
-css <- data[[2]]
+# downliad data
+URL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(url = URL, destfile = "NEI.zip")
+# read files
+data <- unzip(zipfile = "NEI.zip")
+emissions <- readRDS(data[grep(x = data, "summarySCC_PM25.rds")])
+scc <- readRDS(data[grep(x = data, "Source_Classification_Code.rds")])
+
+emissions <- as.data.table(emissions)
+scc <- as.data.table(scc)
 
 # get SCC codes for coal related emissions
 motor.loc = grep(x = scc$Short.Name, "Motor")
@@ -18,9 +27,17 @@ ts2 <- emissions[fips == "06037",][.(scc.index), lapply(.SD,sum), by = "year", .
 setkey(ts1, "year")
 setkey(ts2, "year")
 
+d <- as.data.frame(cbind(Baltimore_city = ts1$Emissions, LA_county = ts2$Emissions))
+d = cbind(melt(d), year = ts1$year)
+
 # plot to PNG device
 png(filename = "plot6.png")
-par(mfrow = c(2,1))
-plot(ts1, main = "Baltimore city", type = "l")
-plot(ts2, main = "LA county", type = "l")
+
+# base:
+# par(mfrow = c(2,1))
+# plot(ts1, main = "Baltimore city", type = "l")
+# plot(ts2, main = "LA county", type = "l")
+
+qplot(year, value, data = d, facets = .~variable) + geom_line()
+
 dev.off()
